692ef26476c96397e77e27b57e0a2a364a1076e5
hoangbmph58845 <sukuwa4014@gmail.com>
2026-03-28 09:36:14 +0700

    Add project files.

---
+++ DuAnBanGiay/MainForm.cs
@@ -0,0 +1,25 @@
+namespace DuAnBanGiay
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlContent_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DuAnBanGiay/UCBanHang.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace DuAnBanGiay
+{
+    partial class UCBanHang
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlLeft = new Panel();
+            pnlRight = new Panel();
+            panel3 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            label12 = new Label();
+            textBox4 = new TextBox();
+            label11 = new Label();
+            comboBox3 = new ComboBox();
+            label9 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            label8 = new Label();
+            textBox3 = new TextBox();
+            label10 = new Label();
+            panel2 = new Panel();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            label7 = new Label();
+            panel1 = new Panel();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            txtMaHD = new TextBox();
+            pnlThongTinHD = new Label();
+            pnlRight.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            pnlLeft.Dock = DockStyle.Left;
+            pnlLeft.Location = new Point(0, 0);
+            pnlLeft.Name = "pnlLeft";
+            pnlLeft.Size = new Size(533, 759);
+            pnlLeft.TabIndex = 0;
+            // 
+            // pnlRight
+            // 
+            pnlRight.Controls.Add(panel3);
+            pnlRight.Controls.Add(panel2);
+            pnlRight.Controls.Add(panel1);
+            pnlRight.Dock = DockStyle.Fill;
+            pnlRight.Location = new Point(533, 0);
+            pnlRight.Name = "pnlRight";
+            pnlRight.Size = new Size(619, 759);
+            pnlRight.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button3);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(textBox5);
+            panel3.Controls.Add(label12);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(label11);
+            panel3.Controls.Add(comboBox3);
+            panel3.Controls.Add(label9);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(label10);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(0, 322);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(619, 437);
+            panel3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(438, 362);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 18;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(266, 362);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 17;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(64, 362);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 16;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.BorderStyle = BorderStyle.None;
+            textBox5.Location = new Point(519, 266);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(97, 24);
+            textBox5.TabIndex = 15;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(519, 225);
+            label12.Name = "label12";
+            label12.Size = new Size(85, 25);
+            label12.TabIndex = 14;
+            label12.Text = "Số lượng";
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Location = new Point(6, 266);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(240, 24);
+            textBox4.TabIndex = 13;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(6, 218);
+            label11.Name = "label11";
+            label11.Size = new Size(196, 32);
+            label11.TabIndex = 12;
+            label11.Text = "KHÁCH CẦN TRẢ";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(284, 160);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(150, 33);
+            comboBox3.TabIndex = 11;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(6, 163);
+            label9.Name = "label9";
+            label9.Size = new Size(206, 25);
+            label9.TabIndex = 10;
+            label9.Text = "Phương thức thanh toán";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(284, 106);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Top;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(0, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(174, 32);
+            label6.TabIndex = 8;
+            label6.Text = "THANH TOÁN";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(6, 106);
+            label8.Name = "label8";
+            label8.Size = new Size(87, 25);
+            label8.TabIndex = 6;
+            label8.Text = "Giảm giá ";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(284, 50);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 1;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 53);
+            label10.Name = "label10";
+            label10.Size = new Size(132, 25);
+            label10.TabIndex = 0;
+            label10.Text = "Tổng tiền hàng";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(comboBox2);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label7);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 161);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(619, 161);
+            panel2.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(134, 106);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(300, 33);
+            comboBox2.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(134, 56);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(300, 33);
+            comboBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Top;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(0, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(176, 32);
+            label4.TabIndex = 8;
+            label4.Text = "KHÁCH HÀNG";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 106);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 25);
+            label5.TabIndex = 6;
+            label5.Text = "Voucher";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 53);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 25);
+            label7.TabIndex = 0;
+            label7.Text = "Khách hàng";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(txtMaHD);
+            panel1.Controls.Add(pnlThongTinHD);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(619, 161);
+            panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(271, 32);
+            label3.TabIndex = 8;
+            label3.Text = "THÔNG TIN HÓA ĐƠN";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(134, 106);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 25);
+            label1.TabIndex = 6;
+            label1.Text = "Ngày lập";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(438, 60);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(310, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Nhân viên";
+            // 
+            // txtMaHD
+            // 
+            txtMaHD.Location = new Point(134, 55);
+            txtMaHD.Name = "txtMaHD";
+            txtMaHD.ReadOnly = true;
+            txtMaHD.Size = new Size(150, 31);
+            txtMaHD.TabIndex = 1;
+            // 
+            // pnlThongTinHD
+            // 
+            pnlThongTinHD.AutoSize = true;
+            pnlThongTinHD.Location = new Point(6, 53);
+            pnlThongTinHD.Name = "pnlThongTinHD";
+            pnlThongTinHD.Size = new Size(109, 25);
+            pnlThongTinHD.TabIndex = 0;
+            pnlThongTinHD.Text = "Mã hóa đơn";
+            // 
+            // UCBanHang
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(pnlRight);
+            Controls.Add(pnlLeft);
+            Name = "UCBanHang";
+            Size = new Size(1152, 759);
+            pnlRight.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlLeft;
+        private Panel pnlRight;
+        private Panel panel1;
+        private Label pnlThongTinHD;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox txtMaHD;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Panel panel3;
+        private ComboBox comboBox3;
+        private Label label9;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label8;
+        private TextBox textBox3;
+        private Label label10;
+        private Panel panel2;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox5;
+        private Label label12;
+        private TextBox textBox4;
+        private Label label11;
+    }
+}
