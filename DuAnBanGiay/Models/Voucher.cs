28b70f7b2359ed8c42cbf7e6f8ee7a91c46774cb
hoangbmph58845 <sukuwa4014@gmail.com>
2026-04-08 09:39:33 +0700

    Revert "Hoa don"

---
+++ DuAnBanGiay/Models/Voucher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuAnBanGiay.Models;
+
+public partial class Voucher
+{
+    public int MaVoucher { get; set; }
+
+    public string? MaCode { get; set; }
+
+    public decimal? SoTienGiam { get; set; }
+
+    public decimal? DieuKienGiam { get; set; }
+
+    public int? TrangThai { get; set; }
+
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+}
