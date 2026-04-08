ec3fc1f2671e57d74daddd811e43f99a0b1ee533
PH30906 <tientdph30906@fpt.edu.vn>
2026-04-08 07:57:30 +0700

    Remove redundant lab and test folders

--- Baithuchanh01/Baithuchanh01/Dtos/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Baithuchanh01.Dtos
-{
-    public class CategoryDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string? Description { get; set; }
-    }
-}
