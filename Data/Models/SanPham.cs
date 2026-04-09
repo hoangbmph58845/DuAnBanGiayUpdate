8b38f03c100a40f8f7b17d6710c9b9617d01ad21
PH30906 <tientdph30906@fpt.edu.vn>
2026-04-09 21:14:28 +0700

    update hoa don

---
+++ Data/Models/SanPham.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinFormsDashboard.Data.Models;
+
+public partial class SanPham
+{
+    public int MaSanPham { get; set; }
+
+    public string TenSP { get; set; } = null!;
+
+    public int? MaThuongHieu { get; set; }
+
+    public int? MaTheLoai { get; set; }
+
+    public int? MaNCC { get; set; }
+
+    public int? MaChatLieu { get; set; }
+
+    public int? TrangThai { get; set; }
+
+    public virtual ICollection<ChiTietSanPham> ChiTietSanPhams { get; set; } = new List<ChiTietSanPham>();
+
+    public virtual ChatLieu? MaChatLieuNavigation { get; set; }
+
+    public virtual NhaCungCap? MaNCCNavigation { get; set; }
+
+    public virtual TheLoai? MaTheLoaiNavigation { get; set; }
+
+    public virtual ThuongHieu? MaThuongHieuNavigation { get; set; }
+}
