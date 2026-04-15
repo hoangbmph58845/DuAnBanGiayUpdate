5b35cebc5753809d5803e9c66d14d9544ad423a1
hoangbmph58845 <sukuwa4014@gmail.com>
2026-04-15 08:25:58 +0700

    update designer files

--- DuAnBanGiay/UCBanHang.Designer.cs
+++ DuAnBanGiay/UCBanHang.Designer.cs
@@ -69,9 +69,9 @@
             // 
             pnlLeft.Dock = DockStyle.Left;
             pnlLeft.Location = new Point(0, 0);
-            pnlLeft.Margin = new Padding(2, 2, 2, 2);
+            pnlLeft.Margin = new Padding(2);
             pnlLeft.Name = "pnlLeft";
-            pnlLeft.Size = new Size(426, 607);
+            pnlLeft.Size = new Size(532, 759);
             pnlLeft.TabIndex = 0;
             // 
             // pnlRight
@@ -80,10 +80,10 @@
             pnlRight.Controls.Add(panel2);
             pnlRight.Controls.Add(panel1);
             pnlRight.Dock = DockStyle.Fill;
-            pnlRight.Location = new Point(426, 0);
-            pnlRight.Margin = new Padding(2, 2, 2, 2);
+            pnlRight.Location = new Point(532, 0);
+            pnlRight.Margin = new Padding(2);
             pnlRight.Name = "pnlRight";
-            pnlRight.Size = new Size(496, 607);
+            pnlRight.Size = new Size(987, 759);
             pnlRight.TabIndex = 1;
             // 
             // panel3
@@ -103,38 +103,38 @@
             panel3.Controls.Add(textBox3);
             panel3.Controls.Add(label10);
             panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(0, 258);
-            panel3.Margin = new Padding(2, 2, 2, 2);
+            panel3.Location = new Point(0, 322);
+            panel3.Margin = new Padding(2);
             panel3.Name = "panel3";
-            panel3.Size = new Size(496, 349);
+            panel3.Size = new Size(987, 437);
             panel3.TabIndex = 3;
             // 
             // button3
             // 
-            button3.Location = new Point(350, 290);
-            button3.Margin = new Padding(2, 2, 2, 2);
+            button3.Location = new Point(438, 362);
+            button3.Margin = new Padding(2);
             button3.Name = "button3";
-            button3.Size = new Size(90, 27);
+            button3.Size = new Size(112, 34);
             button3.TabIndex = 18;
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(213, 290);
-            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Location = new Point(266, 362);
+            button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(90, 27);
+            button2.Size = new Size(112, 34);
             button2.TabIndex = 17;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(51, 290);
-            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Location = new Point(64, 362);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(90, 27);
+            button1.Size = new Size(112, 34);
             button1.TabIndex = 16;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -142,70 +142,70 @@
             // textBox5
             // 
             textBox5.BorderStyle = BorderStyle.None;
-            textBox5.Location = new Point(415, 213);
-            textBox5.Margin = new Padding(2, 2, 2, 2);
+            textBox5.Location = new Point(519, 266);
+            textBox5.Margin = new Padding(2);
             textBox5.Name = "textBox5";
             textBox5.ReadOnly = true;
-            textBox5.Size = new Size(78, 20);
+            textBox5.Size = new Size(98, 24);
             textBox5.TabIndex = 15;
             // 
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(415, 180);
+            label12.Location = new Point(519, 225);
             label12.Margin = new Padding(2, 0, 2, 0);
             label12.Name = "label12";
-            label12.Size = new Size(69, 20);
+            label12.Size = new Size(85, 25);
             label12.TabIndex = 14;
             label12.Text = "Số lượng";
             // 
             // textBox4
             // 
             textBox4.BorderStyle = BorderStyle.None;
-            textBox4.Location = new Point(5, 213);
-            textBox4.Margin = new Padding(2, 2, 2, 2);
+            textBox4.Location = new Point(6, 266);
+            textBox4.Margin = new Padding(2);
             textBox4.Name = "textBox4";
             textBox4.ReadOnly = true;
-            textBox4.Size = new Size(192, 20);
+            textBox4.Size = new Size(240, 24);
             textBox4.TabIndex = 13;
             // 
             // label11
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label11.Location = new Point(5, 174);
+            label11.Location = new Point(6, 218);
             label11.Margin = new Padding(2, 0, 2, 0);
             label11.Name = "label11";
-            label11.Size = new Size(162, 28);
+            label11.Size = new Size(196, 32);
             label11.TabIndex = 12;
             label11.Text = "KHÁCH CẦN TRẢ";
             // 
             // comboBox3
             // 
             comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(227, 128);
-            comboBox3.Margin = new Padding(2, 2, 2, 2);
+            comboBox3.Location = new Point(284, 160);
+            comboBox3.Margin = new Padding(2);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(121, 28);
+            comboBox3.Size = new Size(150, 33);
             comboBox3.TabIndex = 11;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(5, 130);
+            label9.Location = new Point(6, 162);
             label9.Margin = new Padding(2, 0, 2, 0);
             label9.Name = "label9";
-            label9.Size = new Size(168, 20);
+            label9.Size = new Size(206, 25);
             label9.TabIndex = 10;
             label9.Text = "Phương thức thanh toán";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(227, 85);
-            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Location = new Point(284, 106);
+            textBox2.Margin = new Padding(2);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(121, 27);
+            textBox2.Size = new Size(150, 31);
             textBox2.TabIndex = 9;
             // 
             // label6
@@ -216,36 +216,36 @@
             label6.Location = new Point(0, 0);
             label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(146, 28);
+            label6.Size = new Size(174, 32);
             label6.TabIndex = 8;
             label6.Text = "THANH TOÁN";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(5, 85);
+            label8.Location = new Point(6, 106);
             label8.Margin = new Padding(2, 0, 2, 0);
             label8.Name = "label8";
-            label8.Size = new Size(73, 20);
+            label8.Size = new Size(87, 25);
             label8.TabIndex = 6;
             label8.Text = "Giảm giá ";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(227, 40);
-            textBox3.Margin = new Padding(2, 2, 2, 2);
+            textBox3.Location = new Point(284, 50);
+            textBox3.Margin = new Padding(2);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(121, 27);
+            textBox3.Size = new Size(150, 31);
             textBox3.TabIndex = 1;
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(5, 42);
+            label10.Location = new Point(6, 52);
             label10.Margin = new Padding(2, 0, 2, 0);
             label10.Name = "label10";
-            label10.Size = new Size(109, 20);
+            label10.Size = new Size(132, 25);
             label10.TabIndex = 0;
             label10.Text = "Tổng tiền hàng";
             // 
@@ -257,28 +257,28 @@
             panel2.Controls.Add(label5);
             panel2.Controls.Add(label7);
             panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(0, 129);
-            panel2.Margin = new Padding(2, 2, 2, 2);
+            panel2.Location = new Point(0, 161);
+            panel2.Margin = new Padding(2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(496, 129);
+            panel2.Size = new Size(987, 161);
             panel2.TabIndex = 2;
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(107, 85);
-            comboBox2.Margin = new Padding(2, 2, 2, 2);
+            comboBox2.Location = new Point(134, 106);
+            comboBox2.Margin = new Padding(2);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(241, 28);
+            comboBox2.Size = new Size(300, 33);
             comboBox2.TabIndex = 10;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(107, 45);
-            comboBox1.Margin = new Padding(2, 2, 2, 2);
+            comboBox1.Location = new Point(134, 56);
+            comboBox1.Margin = new Padding(2);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(241, 28);
+            comboBox1.Size = new Size(300, 33);
             comboBox1.TabIndex = 9;
             // 
             // label4
@@ -289,27 +289,27 @@
             label4.Location = new Point(0, 0);
             label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(146, 28);
+            label4.Size = new Size(176, 32);
             label4.TabIndex = 8;
             label4.Text = "KHÁCH HÀNG";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(5, 85);
+            label5.Location = new Point(6, 106);
             label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(62, 20);
+            label5.Size = new Size(76, 25);
             label5.TabIndex = 6;
             label5.Text = "Voucher";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(5, 42);
+            label7.Location = new Point(6, 52);
             label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(86, 20);
+            label7.Size = new Size(104, 25);
             label7.TabIndex = 0;
             label7.Text = "Khách hàng";
             // 
@@ -324,9 +324,9 @@
             panel1.Controls.Add(pnlThongTinHD);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
-            panel1.Margin = new Padding(2, 2, 2, 2);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(496, 129);
+            panel1.Size = new Size(987, 161);
             panel1.TabIndex = 1;
             // 
             // label3
@@ -337,76 +337,76 @@
             label3.Location = new Point(0, 0);
             label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(227, 28);
+            label3.Size = new Size(271, 32);
             label3.TabIndex = 8;
             label3.Text = "THÔNG TIN HÓA ĐƠN";
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(107, 85);
-            dateTimePicker1.Margin = new Padding(2, 2, 2, 2);
+            dateTimePicker1.Location = new Point(134, 106);
+            dateTimePicker1.Margin = new Padding(2);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(241, 27);
+            dateTimePicker1.Size = new Size(300, 31);
             dateTimePicker1.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(5, 85);
+            label1.Location = new Point(6, 106);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(69, 20);
+            label1.Size = new Size(83, 25);
             label1.TabIndex = 6;
             label1.Text = "Ngày lập";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(350, 48);
-            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Location = new Point(438, 60);
+            textBox1.Margin = new Padding(2);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
-            textBox1.Size = new Size(121, 27);
+            textBox1.Size = new Size(150, 31);
             textBox1.TabIndex = 5;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(248, 46);
+            label2.Location = new Point(310, 58);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(75, 20);
+            label2.Size = new Size(91, 25);
             label2.TabIndex = 4;
             label2.Text = "Nhân viên";
             // 
             // txtMaHD
             // 
-            txtMaHD.Location = new Point(107, 44);
-            txtMaHD.Margin = new Padding(2, 2, 2, 2);
+            txtMaHD.Location = new Point(134, 55);
+            txtMaHD.Margin = new Padding(2);
             txtMaHD.Name = "txtMaHD";
             txtMaHD.ReadOnly = true;
-            txtMaHD.Size = new Size(121, 27);
+            txtMaHD.Size = new Size(150, 31);
             txtMaHD.TabIndex = 1;
             // 
             // pnlThongTinHD
             // 
             pnlThongTinHD.AutoSize = true;
-            pnlThongTinHD.Location = new Point(5, 42);
+            pnlThongTinHD.Location = new Point(6, 52);
             pnlThongTinHD.Margin = new Padding(2, 0, 2, 0);
             pnlThongTinHD.Name = "pnlThongTinHD";
-            pnlThongTinHD.Size = new Size(89, 20);
+            pnlThongTinHD.Size = new Size(109, 25);
             pnlThongTinHD.TabIndex = 0;
             pnlThongTinHD.Text = "Mã hóa đơn";
             // 
             // UCBanHang
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             Controls.Add(pnlRight);
             Controls.Add(pnlLeft);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "UCBanHang";
-            Size = new Size(922, 607);
+            Size = new Size(1519, 759);
             pnlRight.ResumeLayout(false);
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
